ca5bbeeb28c872593e5e2fb16f830e7250ccab66
Juliette <git@juliette.page>
2020-11-19 19:23:36 -0500

    Settings groundwork

---
+++ Prefs/Prefs.h
@@ -0,0 +1,5 @@
+#import <Preferences/PSListController.h>
+
+@interface JulietteCCSettings : PSListController
+
+@end
---
+++ Prefs/Prefs.m
@@ -0,0 +1,11 @@
+#include "Prefs.h"
+
+@implementation JulietteCCSettings 
+
+    - (NSArray *)specifiers {
+        if (!_specifiers) {
+            _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+        }
+        return _specifiers;
+    }
+@end

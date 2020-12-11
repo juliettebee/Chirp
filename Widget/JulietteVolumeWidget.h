b08903eb06b269f01364c619f8366fba2027b2b9
Juliette <git@juliette.page>
2020-12-11 16:53:20 -0500

    Custom directions!

--- Widget/JulietteVolumeWidget.h
+++ Widget/JulietteVolumeWidget.h
@@ -1,12 +1,17 @@
 #import <UIKit/UIKit.h>
-
+#import <Cephei/HBPreferences.h>
 #import <MediaPlayer/MediaPlayer.h>
-@interface JulietteVolumeWidget : UIView
+
+@interface JulietteVolumeWidget : UIView {
+    HBPreferences *preferences;
+}
 @property (nonatomic, retain) UIImageView *albumArt;
--(void)nextSong:(UISwipeGestureRecognizer*) gesture;
--(void)prevSong:(UISwipeGestureRecognizer*) gesture;
--(void)playSong:(UISwipeGestureRecognizer*) gesture;
+-(void)nextSong:(UISwipeGestureRecognizer*) gesture; 
+-(void)prevSong:(UISwipeGestureRecognizer*) gesture; 
 -(void)pauseSong:(UISwipeGestureRecognizer*) gesture; 
--(void) songChangeNotification:(NSNotification *) notification; 
--(void)updateArtwork;
+-(void)playSong:(UISwipeGestureRecognizer*) gesture; 
+-(void)songChangeNotification:(NSNotification *) notification; 
+-(void)updateArtwork; 
+-(void)getPreferences; 
+-(SEL)getAction:(NSInteger*)key; 
 @end
--- Widget/JulietteVolumeWidget.m
+++ Widget/JulietteVolumeWidget.m
@@ -16,25 +16,10 @@
         //Rounding corners
         [[self layer] setCornerRadius:10];
         self.layer.masksToBounds = TRUE;
-        // Adding next song gesture
-        UISwipeGestureRecognizer *nextSong = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(nextSong:)]; // Creating with the target of self and the action of next song, that method gets called when the swipe is made.
-        [nextSong setDirection:(UISwipeGestureRecognizerDirectionRight)]; // Setting direction of the swipe
-        [self addGestureRecognizer:nextSong]; // Adding the gesture to the widget
-        // Adding pause gesture
-        UISwipeGestureRecognizer *pauseSong = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(pauseSong:)];
-        [pauseSong setDirection:(UISwipeGestureRecognizerDirectionUp)];
-        [self addGestureRecognizer:pauseSong];
-        // Adding play gesture
-        UISwipeGestureRecognizer *playSong = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(playSong:)];
-        [playSong setDirection:(UISwipeGestureRecognizerDirectionDown)];
-        [self addGestureRecognizer:playSong];
-        // Adding previous song gesture
-        UISwipeGestureRecognizer *prevSong = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(prevSong:)];
-        [prevSong setDirection:(UISwipeGestureRecognizerDirectionLeft)];
-        [self addGestureRecognizer:prevSong];
+        // Getting prefs
+        [self getPreferences];
         // Adding view
         [self addSubview:self.albumArt];
-
         // Subscribing to a notification that gets posted when a song changes 
         [[NSNotificationCenter defaultCenter] addObserver:self
         selector:@selector(songChangeNotification:) 
@@ -58,12 +43,61 @@
 -(void)playSong:(UISwipeGestureRecognizer*) gesture {
     [MPMusicPlayerController.systemMusicPlayer play];
 }
-- (void) songChangeNotification:(NSNotification *) notification {
+- (void)songChangeNotification:(NSNotification *) notification {
     [self updateArtwork];
 }
 // Dedicated function to update artwork 
 -(void)updateArtwork {
     MPMediaItemArtwork *art = MPMusicPlayerController.systemMusicPlayer.nowPlayingItem.artwork;
     [self.albumArt setImage:[art imageWithSize:self.frame.size]];
+}
+// Function to get prefs
+-(void)getPreferences {
+    preferences = [[HBPreferences alloc] initWithIdentifier:@"page.juliette.CCMusicSliderPrefs"];
+
+    // Creation and registration of variables
+    NSInteger *up;
+    [preferences registerInteger:&up default:1 forKey:@"actionUp"];
+    NSInteger *down;
+    [preferences registerInteger:&down default:0 forKey:@"actionDown"];
+    NSInteger *left;
+    [preferences registerInteger:&left default:3 forKey:@"actionLeft"];
+    NSInteger *right;
+    [preferences registerInteger:&right default:4 forKey:@"actionRight"];
+    // Setting gestures
+    UISwipeGestureRecognizer *upGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:[self getAction:up]];
+    UISwipeGestureRecognizer *downGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:[self getAction:down]];
+    UISwipeGestureRecognizer *leftGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:[self getAction:left]];
+    UISwipeGestureRecognizer *rightGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:[self getAction:right]];
+    // Setting directions
+    [upGesture setDirection:(UISwipeGestureRecognizerDirectionUp)];
+    [downGesture setDirection:(UISwipeGestureRecognizerDirectionDown)];
+    [leftGesture setDirection:(UISwipeGestureRecognizerDirectionLeft)];
+    [rightGesture setDirection:(UISwipeGestureRecognizerDirectionRight)];
+    // Adding
+    [self addGestureRecognizer:upGesture];
+    [self addGestureRecognizer:downGesture];
+    [self addGestureRecognizer:leftGesture];
+    [self addGestureRecognizer:rightGesture];
+
+}
+-(SEL)getAction:(NSInteger*)key {
+    switch((int)key) {
+        case 0:
+            return @selector(playSong:);
+            break;
+        case 1:
+            return @selector(pauseSong:);
+            break;
+        case 2:
+            return @selector(nextSong:);
+            break;
+        case 3:
+            return @selector(prevSong:);
+            break;
+        default:
+            return @selector(playSong:);
+            break;
+    }
 }
 @end

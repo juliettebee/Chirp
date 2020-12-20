28d6942f1a4aca36b0802b46deac2a6f8903b074
Juliette <git@juliette.page>
2020-12-20 17:47:24 -0500

    Generating actions instead of hand making!

--- Prefs/Prefs.h
+++ Prefs/Prefs.h
@@ -5,5 +5,13 @@
 @end
 
 @interface JulietteCCSettingsGesture : PSListController
+@end
 
+@interface PSSpecifier : NSObject 
+@property (nonatomic,retain) NSArray * values; 
+@property (nonatomic,retain) NSDictionary * titleDictionary;    
+@property (nonatomic,retain) NSDictionary * shortTitleDictionary;
++(id)preferenceSpecifierNamed:(id)arg1 target:(id)arg2 set:(SEL)arg3 get:(SEL)arg4 detail:(Class)arg5 cell:(long long)arg6 edit:(Class)arg7 ;
+-(void)setProperty:(id)arg1 forKey:(id)arg2 ;
+-(void)setIdentifier:(NSString *)arg1 ;
 @end
